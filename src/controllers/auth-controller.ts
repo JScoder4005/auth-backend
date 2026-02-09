510a9aad5238b3d8e99f892a6b3780cb909c333b
Uday kumar M <136188719+JScoder4005@users.noreply.github.com>
2026-02-09 23:50:01 +0530

    Update auth-controller.ts

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -37,7 +37,7 @@ export const register = async (req: Request, res: Response) => {
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
-
+     
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prisma.user.create({
@@ -46,7 +46,7 @@ export const register = async (req: Request, res: Response) => {
         password: hashedPassword,
       },
     });
-
+    console.log("userReq", user);
     return res.status(201).json({ message: "User created", userId: user.id });
   } catch (err) {
     console.error(err);
